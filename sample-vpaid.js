8014a5c40bac867c0784a51070fc6985b9066b85
BLFabian <46373277+BLFabian@users.noreply.github.com>
2024-11-14 12:06:06 +0100

    Update sample-vpaid.js

--- sample-vpaid.js
+++ sample-vpaid.js
@@ -1,13 +1,14 @@
 (function() {
-    var adContainer, button;
+    var adContainer, button, adDuration = 30, intervalId, remainingTime = adDuration, adPaused = false, adVolume = 1.0;
+    var eventCallbacks = {};
 
     function initAd(width, height, viewMode, desiredBitrate, creativeData, environmentVars) {
         // Create the ad container
         adContainer = document.createElement("div");
-        adContainer.style.position = "relative";
-        adContainer.style.width = "100%";
-        adContainer.style.height = "100%";
-        adContainer.style.overflow = "hidden";
+        adContainer.style.position = "absolute";
+        adContainer.style.width = width + "px";
+        adContainer.style.height = height + "px";
+        adContainer.style.backgroundColor = "#000";
         document.body.appendChild(adContainer);
 
         // Create the button
@@ -27,49 +28,103 @@
 
         button.addEventListener("click", function() {
             window.open("https://example.com/clickthrough", "_blank");
+            if (typeof eventCallbacks['AdClickThru'] === 'function') {
+                eventCallbacks['AdClickThru']('https://example.com/clickthrough', '0', true);
+            }
         });
 
         adContainer.appendChild(button);
+
+        // Notify that the ad has been loaded
+        if (typeof eventCallbacks['AdLoaded'] === 'function') {
+            eventCallbacks['AdLoaded']();
+        }
     }
 
     function startAd() {
-        console.log("Ad started");
+        // Start the ad logic
+        if (typeof eventCallbacks['AdStarted'] === 'function') {
+            eventCallbacks['AdStarted']();
+        }
+
+        // Simulate ad duration
+        intervalId = setInterval(function() {
+            if (!adPaused) {
+                remainingTime -= 1;
+                if (remainingTime <= 0) {
+                    stopAd();
+                }
+            }
+        }, 1000);
     }
 
     function stopAd() {
+        // Clean up the ad
+        clearInterval(intervalId);
         if (adContainer) {
             document.body.removeChild(adContainer);
         }
-        console.log("Ad stopped");
-    }
-
-    function skipAd() {
-        stopAd();
-    }
-
-    function resizeAd(width, height, viewMode) {
-        console.log("Ad resized");
+        if (typeof eventCallbacks['AdStopped'] === 'function') {
+            eventCallbacks['AdStopped']();
+        }
     }
 
     function pauseAd() {
-        console.log("Ad paused");
+        adPaused = true;
+        if (typeof eventCallbacks['AdPaused'] === 'function') {
+            eventCallbacks['AdPaused']();
+        }
     }
 
     function resumeAd() {
-        console.log("Ad resumed");
+        adPaused = false;
+        if (typeof eventCallbacks['AdPlaying'] === 'function') {
+            eventCallbacks['AdPlaying']();
+        }
     }
 
-    // Export the VPAID interface functions
+    function getAdDuration() {
+        return adDuration;
+    }
+
+    function getAdRemainingTime() {
+        return remainingTime;
+    }
+
+    function setAdVolume(volume) {
+        adVolume = volume;
+        if (typeof eventCallbacks['AdVolumeChanged'] === 'function') {
+            eventCallbacks['AdVolumeChanged']();
+        }
+    }
+
+    function getAdVolume() {
+        return adVolume;
+    }
+
+    function subscribe(callback, eventName) {
+        eventCallbacks[eventName] = callback;
+    }
+
+    function unsubscribe(eventName) {
+        delete eventCallbacks[eventName];
+    }
+
+    // Export the VPAID interface
     window.getVPAIDAd = function() {
         return {
             handshakeVersion: function(version) { return "2.0"; },
             initAd: initAd,
             startAd: startAd,
             stopAd: stopAd,
-            skipAd: skipAd,
-            resizeAd: resizeAd,
             pauseAd: pauseAd,
-            resumeAd: resumeAd
+            resumeAd: resumeAd,
+            getAdDuration: getAdDuration,
+            getAdRemainingTime: getAdRemainingTime,
+            setAdVolume: setAdVolume,
+            getAdVolume: getAdVolume,
+            subscribe: subscribe,
+            unsubscribe: unsubscribe
         };
     };
 })();
